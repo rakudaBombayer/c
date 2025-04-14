b127fe4cc54737840bb5feb1a0f2124a3a505ce3
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-04-14 10:02:22 +0900

    Create list6-11.c

---
+++ list6-11.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define NUMBER 5
+
+
+int max_of(int v[], int n)
+{
+
+	int max = v[0];
+
+	for (int i = 1; i < n; i++)
+		if (v[i] > max)
+			max = v[i];
+
+	return max;
+
+}
+
+
+
+
+int main(void)
+{
+	int eng[NUMBER];
+	int mat[NUMBER];
+
+	printf("%d人の点数を入力せよ。\n", NUMBER);
+	for (int i = 0; i < NUMBER; i++){
+		printf("[%d] 英語：", i +1);
+		fflush(0);
+		scanf("%d", &eng[i]);
+
+		printf("   数学 :");
+		fflush(0);
+		scanf("%d", &mat[i]);
+
+	}
+
+	int max_e = max_of(eng, NUMBER);
+	int max_m = max_of(mat, NUMBER);
+
+	printf("英語の最高点=%d\n", max_e);
+	printf("英語の最高点=%d\n", max_m);
+
+	return 0;
+}
+
+
