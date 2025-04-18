fcffa9f12c5cae4048c71777185ab3825286d4d2
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-04-18 13:26:03 +0900

    Create list9-7.c

---
+++ list9-7.c
@@ -0,0 +1,14 @@
+// Ex0810.c
+#include <stdio.h>
+
+int main(void)
+{
+	char s[][6] = {"Turbo", "NA", "DOHC"};
+
+	for (int i = 0; i < 3; i++)
+		printf("s[%d] = \"%s\"\n", i, s[i]);
+
+
+	return 0;
+}
+
