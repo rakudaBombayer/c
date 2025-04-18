80d8b2916f9c7af95f57bb96c7faaa9d54068ba8
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-04-18 10:49:26 +0900

    Create list8-10.c

---
+++ list8-10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+
+    printf("EOF = %d\n", EOF);
+    fflush(0);
+    printf("1 = %d\n", '1');
+    fflush(0);
+    printf("1 = %d\n", '2');
+    fflush(0);
+    printf("1 = %d\n", '3');
+    fflush(0);
+    printf("1 = %d\n", '4');
+    fflush(0);
+    printf("1 = %d\n", '5');
+    fflush(0);
+    printf("1 = %d\n", '6');
+    fflush(0);
+    printf("1 = %d\n", '7');
+    fflush(0);
+    printf("1 = %d\n", '8');
+    fflush(0);
+    printf("1 = %d\n", '9');
+    fflush(0);
+
+
+    return 0;
+}
