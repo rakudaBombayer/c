98bca99ad194587a0bc778433f286c089135334c
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-04-22 12:14:16 +0900

    Update e9-10.c

--- e9-10.c
+++ e9-10.c
@@ -4,16 +4,19 @@
 
 void del_digit(char s[])
 {
-    int i = 0;
-    while (s[i]) {
-        if (s[i] >= '0' && s[i] <= '9') {
-            s[i] = '\0';
-            putchar(s[i++]);
-        } else {
-            putchar(s[i++]);
-        }
+	int i = 0;
+	while(s[i]){
+		if(s[i] >= '0' && s[i] <= '9'){
+			s[i] = '\0';
+//			putchar(s[i++]);
+			i++;
 
-    }
+		}else{
+			putchar(s[i++]);
+		}
+
+
+	}
     putchar('\n');
 }
 
