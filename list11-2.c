ca7d836989cd2fe265462bbf1a01158e852e6697
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-04-24 11:49:01 +0900

    Create list11-2.c

---
+++ list11-2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//int main(void)
+//{
+//	char s[] = "ABC";
+//
+//	printf("s = \"%s\"\n", s);
+//
+//	s = "DEF";
+//
+//	printf("s = \"%s\"\n", s);
+//
+//
+//	return 0;
+//
+//}
+
+int main(void)
+{
+	char *p = "123";
+
+	printf("p = \"%s\"\n", p);
+
+	p = "456";
+
+	printf("p = \"%s\"\n", p);
+
+
+	return 0;
+
+}
