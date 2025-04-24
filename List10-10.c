42294c7822be1ae3dee731cb12ff46154a0bbb71
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-04-24 10:24:13 +0900

    Create List10-10.c

---
+++ List10-10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(void){
+	int a[5] = {1, 2, 3, 4, 5};
+
+	int *p = a;
+
+	for (int i =0; i < 5; i++)
+		printf("a[%d] = %d  *(a+%d) = %d  p[%d] = %d *(p+%d) = %d\n",
+				i , a[i], i, *(a +i), i, p[i], i, *(p + i));
+
+
+	for (int i =0; i < 5; i++)
+		printf("a[%d] = %p  a+%d = %p  p[%d] = %p p+%d = %p\n",
+				i , &a[i], i, (a +i), i, &p[i], i, *(p + i));
+
+
+
+	return 0;
+
+}
